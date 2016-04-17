493c48adfb5c3384eafabaa674864f1be7cea9e3
samlyu <sen.lyu>
2016-04-17 20:54:44 +0800

    I did it

---
+++ GuessGame.java
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: GuessGame.java
+	> Author: 
+	> Mail: 
+	> Created Time: Sun 17 Apr 2016 08:50:21 PM CST
+ ************************************************************************/
+
+public class GuessGame
+{
+    Player p1;
+    Player p2;
+    Player p3;
+
+    public void startGame() {
+        p1 = new Player();
+        p2 = new Player();
+        p3 = new Player();
+
+    }
+}
