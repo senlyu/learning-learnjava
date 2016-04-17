a811a4bcd50fde8eb263f75712a459d213838096
samlyu <sen.lyu@uconn.edu>
2016-04-17 21:43:55 +0800

    GuessGame Launcher. And remmber you could spell wrong at the class name

---
+++ GameLauncher.java
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: GameLancher.java
+	> Author: 
+	> Mail: 
+	> Created Time: Sun 17 Apr 2016 09:37:16 PM CST
+ ************************************************************************/
+
+public class GameLauncher
+{
+    public static void main (String[] args) {
+        GuessGame game= new GuessGame();
+        game.startGame();
+    }
+}
