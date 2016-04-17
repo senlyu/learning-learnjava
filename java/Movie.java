2db7d59af3de73bd6d07ec53faec1f9da82b2530
SenLyu <sen.lyu@uconn.edu>
2016-04-17 21:52:09 +0800

    changes the r

---
+++ java/Movie.java
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: Movie.java
+	> Author: 
+	> Mail: 
+	> Created Time: Sun 17 Apr 2016 08:22:02 PM CST
+ ************************************************************************/
+
+public class Movie
+{
+    String title;
+    String genre;
+    int rating;
+
+    void playIt(){
+        System.out.println("Playing the movie "+title);
+
+    }
+}
+
