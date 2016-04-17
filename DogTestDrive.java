0d491dd87525146e1e62b8c8959c5edf07e7c529
samlyu <sen.lyu>
2016-04-17 20:26:27 +0800

    New files

---
+++ DogTestDrive.java
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: DogTestDrive.java
+	> Author: 
+	> Mail: 
+	> Created Time: Sun 17 Apr 2016 08:18:13 PM CST
+ ************************************************************************/
+
+public class DogTestDrive
+{
+    public static void main (String[] args)
+    {
+        Dog d = new Dog();
+        d.size = 40;
+        d.bark();
+    }
+}
---
+++ MovieTestDrive.java
@@ -0,0 +1,9 @@
+/*************************************************************************
+	> File Name: MovieTestDrive.java
+	> Author: 
+	> Mail: 
+	> Created Time: Sun 17 Apr 2016 08:24:43 PM CST
+ ************************************************************************/
+
+public class MovieTestDrive
+
